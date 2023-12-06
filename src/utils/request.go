0304bc01c422f5b78b4be09ebd67aa554a79b495
Emmanuel <emmanuelchan809@gmail.com>
2023-12-06 15:24:41 -0500

    Added public requests

--- src/utils/request.go
+++ src/utils/request.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	"encoding/json"
+)
+
+func (client *Client) SendPublicRequest(request string, params map[string]string) string {
+	response, _ := json.Marshal(params)
+	return string(response)
+}
+
+func (client *Client) SendSignedRequest(request string, params map[string]string) string {
+	return ""
+}
